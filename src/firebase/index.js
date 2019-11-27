2684c0d2d86ba9add3e39348f59eeafb06a80085
Navi <rodrigo@lajous.com.ar>
2019-11-26 23:16:34 -0300

    moved tokens to .env and changed acces values

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBh0EzznlqPRf4hdm7LrFntoiCqTLXf218',
-  authDomain: 'quipu-8fca8.firebaseapp.com',
-  databaseURL: 'https://quipu-8fca8.firebaseio.com',
-  projectId: 'quipu-8fca8',
-  storageBucket: 'quipu-8fca8.appspot.com',
-  messagingSenderId: '282054120629',
-  appId: '1:282054120629:web:38a2dd11cfc1b2314e5afc',
-  measurementId: 'G-ST8HC36S8K'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
